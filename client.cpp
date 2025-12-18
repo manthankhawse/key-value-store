e4db609d6dd0a4525ea7813f21e9f589a055a379
manthankhawse <khawsemanthan246@gmail.com>
2025-12-18 23:14:41 +0530

    added protocol parsing

--- client.cpp
+++ client.cpp
@@ -1,49 +1,139 @@
-#include <cstdlib>
 #include <iostream>
+#include <assert.h>
 #include <netinet/in.h>
-#include <sys/socket.h>
+#include <stdint.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <errno.h>
 #include <unistd.h>
-#include <cstring>
-#include <cerrno>
-
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
 using namespace std;
 
-int main() {
+const size_t max_len = 4096;
+
+static void msg(const char* message){
+    cout<<message<<endl;
+}
+
+static void die(const char* message){
+    int err = errno;
+    cout<<"["<<errno<<"] "<<message<<endl;
+    abort();
+}
+
+static int32_t write_full(int fd, char* buf, size_t n){
+    while(n>0){
+        int rv = write(fd, buf, n);
+        if(rv<=0){
+            return -1;
+        }
+
+        n-=rv;
+        buf+=rv;
+    }
+
+    return 0;
+}
+
+static int32_t read_full(int fd, char* buf, int n){
+    while(n>0){
+        int rv = read(fd, buf, n);
+        if(rv<=0){
+            return -1;
+        }
+
+        n-=rv;
+        buf+=rv;
+    }
+
+    return 0;
+}
+
+static int32_t query(int fd, char* text){
+    int len = strlen(text);
+    if(len>max_len){
+        die("TOO LONG");
+    }
+    char wbuf[4+max_len];
+
+    memcpy(wbuf, &len, 4);
+    memcpy(wbuf+4, text, len);
+    int err = write_full(fd, wbuf, 4+len);
+
+    if(err){
+        return err;
+    }
+
+    char rbuf[4+max_len];
+    errno = 0;
+    err = read_full(fd, rbuf, 4);
+
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+
+    int length = 0;
+
+    memcpy(&length, rbuf, 4);
+    if(length > max_len){
+        die("TOO LONG");
+        return -1;
+    }
+
+    err = read_full(fd, rbuf+4, length);
+
+    if (err) {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+    rbuf[4 + length] = '\0';
+
+    cout<<"Server :"<<(rbuf+4)<<endl;
+    return 0;
+
+}
+
+int main(){
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0) {
-        perror("socket");
-        return EXIT_FAILURE;
+    if(fd<0){
+        die("SOCKET() error");
     }
 
-    sockaddr_in addr{};
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(1234);
+    struct sockaddr_in addr{};
     addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    addr.sin_port = htons(1234);
+    addr.sin_family = AF_INET;
 
-    if (connect(fd, (sockaddr*)&addr, sizeof(addr)) < 0) {
-        perror("connect");
-        close(fd);
-        return EXIT_FAILURE;
+    socklen_t addr_len = sizeof(addr);
+
+    int rv = connect(fd, (struct sockaddr*)&addr, addr_len);
+
+    if(rv){
+        die("CONNECT() error");
     }
 
-    const char msg[] = "Hello";
-    ssize_t sent = write(fd, msg, sizeof(msg) - 1);
-    if (sent < 0) {
-        perror("write");
-        close(fd);
-        return EXIT_FAILURE;
+
+    int32_t err = query(fd, (char*)"Hello");
+    if(err){
+        goto L_DONE;
     }
 
-    char rbuf[64]{};
-    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        perror("read");
-        close(fd);
-        return EXIT_FAILURE;
+    err = query(fd, (char*)"Hello2");
+    if(err){
+        goto L_DONE;
     }
 
-    cout << "Server replied: " << rbuf << endl;
+    err = query(fd, (char*)"Hello3");
+    if(err){
+        goto L_DONE;
+    }
 
+L_DONE:
     close(fd);
-    return EXIT_SUCCESS;
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,84 +1,146 @@
-#include <cstdlib>
-#include <iostream>
+#include <asm-generic/socket.h>
+#include <assert.h>
 #include <netinet/in.h>
-#include <sys/socket.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <errno.h>
 #include <unistd.h>
-#include <cstring>
-#include <cerrno>
-
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
 using namespace std;
 
-int process(int connfd) {
-    char rbuf[64]{};
+static void msg(const char* message){
+    cout<<message<<endl;
+}
 
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        perror("read");
-        return -1;
-    }
+static void die(const char* message){
+    int err = errno;
+    cout<<"["<<errno<<"] "<<message<<endl;
+    abort();
+}
 
-    cout << "Client says: " << rbuf << endl;
+const size_t max_len = 4096;
 
-    const char wbuf[] = "World";
-    ssize_t sent = write(connfd, wbuf, sizeof(wbuf) - 1);
-    if (sent < 0) {
-        perror("write");
-        return -1;
+static int32_t read_full(int fd, char* buf, size_t n){
+    while(n>0){
+        ssize_t rv = read(fd, buf, n);
+        if(rv<=0){
+            return -1;
+        }
+
+        assert(rv<=n);
+        n-=rv;
+        buf+=rv;
     }
 
     return 0;
 }
 
-int main() {
+static int32_t write_full(int fd, char* buf, size_t n){
+    while(n>0){
+        int rv = write(fd, buf, n);
+        if(rv<=0){
+            return -1;
+        }
+
+        n-=rv;
+        buf+=rv;
+    }
+
+    return 0;
+}
+
+static int32_t one_request(int connfd){
+    char rbuf[4+max_len+1];
+
+    errno = 0;
+
+    int32_t err = read_full(connfd, rbuf, 4);
+
+    if(err){
+        msg(errno==0 ? "EOF" : "read() error");
+        return err;
+    }
+
+    int len = 0;
+
+    memcpy(&len, rbuf, 4);
+
+    if(len>max_len){
+        msg("too long");
+        return -1;
+    }
+
+    err = read_full(connfd, rbuf+4, len);
+
+    if(err){
+        msg("read() error");
+        return -1;
+    }
+
+    rbuf[4 + len] = '\0';
+
+    cout<<"Client says: "<<(rbuf+4)<<endl;
+
+    const char reply[] = "world";
+
+    char wbuf[4+sizeof(reply)];
+
+    len = strlen(reply);
+
+    memcpy(wbuf, &len, 4);
+    memcpy(wbuf+4, reply, len);
+
+
+    return write_full(connfd, wbuf, 4+len);
+}
+
+int main(){
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0) {
-        perror("socket");
-        return EXIT_FAILURE;
+    if(fd<0){
+        die("SOCKET() error");
     }
 
     int val = 1;
-    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)) < 0) {
-        perror("setsockopt");
-        close(fd);
-        return EXIT_FAILURE;
-    }
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
-    sockaddr_in addr{};
+    struct sockaddr_in addr{};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(1234);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_addr.s_addr = ntohl(INADDR_ANY);
 
-    if (bind(fd, (sockaddr*)&addr, sizeof(addr)) < 0) {
-        perror("bind");
-        close(fd);
-        return EXIT_FAILURE;
+    int rv = bind(fd, (struct sockaddr*)&addr, sizeof(addr));
+    if(rv){
+        die("BIND() error");
     }
 
-    if (listen(fd, 16) < 0) {
-        perror("listen");
-        close(fd);
-        return EXIT_FAILURE;
+    rv = listen(fd, SOMAXCONN);
+
+    if(rv){
+        die("LISTEN() error");
     }
 
-    cout << "Server listening on port 1234" << endl;
+    while(true){
+        struct sockaddr_in client_addr{};
+        size_t addr_len = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr*)&client_addr, (socklen_t*)&addr_len);
 
-    while (true) {
-        sockaddr_in client_addr{};
-        socklen_t len = sizeof(client_addr);
-
-        int connfd = accept(fd, (sockaddr*)&client_addr, &len);
-        if (connfd < 0) {
-            perror("accept");
+        if(connfd < 0){
             continue;
         }
 
-        if (process(connfd) < 0) {
-            cerr << "Client processing failed" << endl;
+        while (true) {
+            int32_t err = one_request(connfd);
+            if (err) {
+                break;
+            }
         }
 
         close(connfd);
     }
 
-    close(fd);
-    return EXIT_SUCCESS;
+    return 0;
 }
